714ffdfd5c9ed83f452167fb49b35a384e77d641
JP <jamesclay121398@gmail.com>
2024-09-15 23:50:38 +0530

    Sorting_Algos

---
+++ Sorting_algos.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// T(O) : Best/Avg/Worst = O(n2);
+void SelectionSort(vector<int>& arr, int& n)
+{
+    for(int i = 0; i<n-1; i++) // no need to go till last ele as it will be already sorted
+    {
+        int minidx = i;
+        for(int j = i; j<n; j++) // from i till last ele
+        {
+            // minidx = arr[j] < arr[minidx] ? i : j;
+            if(arr[j] < arr[minidx])
+                minidx = j;
+        }
+        swap(arr[minidx], arr[i]);
+    }
+}
+
+// T(O) : Worst/avg = O(n2) Best = O(n)
+void BubbleSort(vector<int>& arr, int& n)
+{ // in this, highest number gets pushed to last in each iteration
+    for(int i = n-1; i>=0; i--) 
+    {
+        bool didSwap = false; // if it is swapped atleast once, generally to check if the received array is already sorted
+        for(int j=0; j<=i-1;j++) // the size of arr sorting reduces
+        {
+            if(arr[j] > arr[j+1])
+            {
+                swap(arr[j], arr[j+1]);
+                didSwap = true;
+            }
+        }
+
+        if(!didSwap) // meaning, no swap was done => the array is already sorted at 1st loop
+            break;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {6,4,8,2,9,1,2,7};
+    int n = arr.size();
+
+    // SelectionSort(arr, n);
+    BubbleSort(arr, n);
+
+    for(int i:arr)
+        cout<<i<<" ";
+    return 0;
+}
