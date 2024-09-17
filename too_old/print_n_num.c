a44f8cf1a0c7a9553827277698c178466fc18f99
JP <jamesclay121398@gmail.com>
2024-09-17 23:46:27 +0530

    minorupdate

--- too_old/print_n_num.c
+++ too_old/print_n_num.c
@@ -4,9 +4,11 @@ int main()
     int i,n,fact=1;
     printf("ENter the value of N:");
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    int arr[n+1];
+    arr[0] = 1, arr[1] = 1;
+    for(i=2;i<=n;i++)
     {
-        fact*=i;
-        printf("Factorial=%d\n",fact);
+        arr[i] = arr[i-1] * i;
     }
+        printf("Factorial=%d\n",arr[n]);
 }
