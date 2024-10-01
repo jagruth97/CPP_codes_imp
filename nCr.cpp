7b23594810c91f630379e4a9578a94826d201ff5
JP <jamesclay121398@gmail.com>
2024-10-01 23:27:47 +0530

    nCr_working

--- nCr.cpp
+++ nCr.cpp
@@ -6,8 +6,8 @@ int nCr(int n, int r)
     long long resultant = 1;
     for(int i=0; i<r; i++)
     {
-        resultant *= (n-i);
-        resultant /= (i+1);
+        resultant *= (n-i); // since fact of big num in denom cancels the part of numerator, so multi only with remaining
+        resultant /= (i+1); // divide in reverse -> 3! : 1 * 2 * 3 instead of 3*2*1
     }
     return static_cast<int>(resultant);
 }
